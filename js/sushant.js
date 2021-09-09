e589c5cf06bdb17e738d9bb94883bc182d198678
Sushant Sejwal <87465442+SushantSejwal@users.noreply.github.com>
2021-09-09 17:20:42 +0530

    Add files via upload

--- js/sushant.js
+++ js/sushant.js
@@ -1,14 +1,5 @@
-$(function () {
-    $(".toggle-btn").on("click", function () {
-
-        if ($(".ul-li").hasClass("active")) {
-            $(".ul-li").removeClass("active");
-        } 
-        else {
-            $(".ul-li").addClass("active");
-
-        }
-    })
-
-});
-
+const button = document.querySelector(".toggle-btn"); const line1 = document.querySelector(".line-1"); const line2 = document.querySelector(".line-2"); const scale = document.querySelector(".menu-items"); const ssss = document.querySelectorAll(".item"); const anchor = document.querySelectorAll(".main-name-a"); const ss1 = document.querySelector(".ss-anchor-1"); const ss2= document.querySelector(".ss-anchor-2"); const ss3 = document.querySelector(".ss-anchor-3"); const ss4 = document.querySelector(".ss-anchor-4");
+button.addEventListener("click", () => { toggle(); });
+ssss.forEach((item) => { item.addEventListener("click", () => { if (scale.classList.contains("sushantsejwal")) { toggle(); } }); });
+anchor.forEach((item) => { item.addEventListener("click", () => { if (scale.classList.contains("sushantsejwal")) { toggle(); } }); });
+function toggle() { line1.classList.toggle("sushantsejwal"); line2.classList.toggle("sushantsejwal"); scale.classList.toggle("sushantsejwal"); ss1.classList.toggle("sushantsejwal"); ss2.classList.toggle("sushantsejwal"); ss3.classList.toggle("sushantsejwal"); ss4.classList.toggle("sushantsejwal"); }
